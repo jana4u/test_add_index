97eb5ed4c3298b08025535f92b6f380df74f5b7a
jana4u <jana4u@seznam.cz>
2012-02-20 14:46:55 +0100

    generated rails 3.2.1 app with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAddIndex::Application.config.secret_token = '1857ab754253232afb6e10e74f69978ef6ac60d2da5df72d66665a49f803872b20234ae76cacb81cabffb4e1dcad6d1305b825f1f6da8c1dd5ac62f303d95058'
